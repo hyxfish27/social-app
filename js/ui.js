ef74596fc5cc68b166358dbe8ac79ea0942b1246
hyxfish27 <avicky1515@gmail.com>
2021-04-21 15:55:26 +0800

    delete all files

--- js/ui.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var sidenav = document.querySelectorAll('.sidenav');
-    var modal = document.querySelectorAll('.modal');
-    var fab = document.querySelectorAll('.fixed-action-btn');
-    M.Sidenav.init(sidenav);
-    M.Modal.init(modal);
-    M.FloatingActionButton.init(fab);
-});
